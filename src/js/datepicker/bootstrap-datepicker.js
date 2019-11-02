80cd9fa7e4180219fec0e302bb7b45b959c71747
tarasposatskyi <posatskyitaras@gmail.com>
2019-11-03 01:43:01 +0200

    Implemented wrapper for displaying weekends and holidays

--- src/js/datepicker/bootstrap-datepicker.js
+++ src/js/datepicker/bootstrap-datepicker.js
@@ -372,7 +372,7 @@
 			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
 			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
 			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November!!", "December"],
 			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 		},
 		isLeapYear: function (year) {
